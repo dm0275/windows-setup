c9f685e7f6399652ff8e85b8f21db08f069d00d0
dm0275 <dm0275@gmail.com>
2018-01-15 19:20:28 -0600

    Add files via upload

---
+++ SetupWIndowsMachine.ps1
@@ -0,0 +1,33 @@
+﻿### Setup Windows Machine
+
+$programs = @(
+  "chocolatey-core.extension",
+  "googlechrome",
+  "adobereader",
+  "jre8",
+  "jdk8",
+  "notepadplusplus",
+  "winrar",
+  "vlc",
+  "git",
+  "python",
+  "atom"
+)
+
+function InstallChoco {
+  Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))  
+}
+
+function InstallPackages {
+  if (choco -v -ne $null) {
+    foreach ($program in $programs) {
+      choco install $program
+    }
+  }
+}
+
+function SetupWindows {
+  Set-ExecutionPolicy Bypass -Scope User -Force
+  InstallChoco
+  InstallPackages
+}
